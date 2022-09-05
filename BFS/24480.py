7d6f126e2a1d3f482ed78401e2f597059b517828
parksey <parkseyeon99@naver.com>
2022-09-05 23:40:43 +0900

    2022-09-05

---
+++ BFS/24480.py
@@ -0,0 +1,38 @@
+N,M,R = map(int,input().split())
+
+node = {}
+for i in range(M):
+    u, v = map(int, input().split())
+    
+    if u not in node:
+        node[u] = [v]
+    else:
+        node[u].append(v)
+    if v not in node:
+        node[v] = [u]
+    else:
+        node[v].append(u)
+    
+     
+for k in node:
+    node[k] = sorted(node[k])
+
+count = 0
+visit=[0 for i in range(N)]
+
+stack = [R]
+while stack:
+    top = stack.pop()
+    if visit[top-1] == 0:
+        count +=1
+        visit[top-1] = count
+
+    if top not in node:
+        continue
+
+    for n in node[top]:
+        if visit[n-1] == 0:
+            stack.append(n)
+
+for i in visit:
+    print(i)
--- dinamic/12865.py
+++ dinamic/12865.py
@@ -13,7 +13,7 @@ def find():
         for j in range(1,K+1):
             if data[i-1][0] <= j:
                 dp[i][j] = max(data[i-1][1]+dp[i-1][j-data[i-1][0]], dp[i-1][j])
-    return 1
-
+            else:
+                dp[i][j] = dp[i-1][j]
 find()
 print(dp[-1][-1])
