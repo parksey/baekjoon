082f04bb3924b7eae6fc318168862de62dc4058e
parksey <parkseyeon99@naver.com>
2022-09-20 23:30:01 +0900

    2022-09-20

---
+++ BFS/1753.py
@@ -0,0 +1,44 @@
+from collections import deque
+import sys
+
+V,E = map(int, input().split())
+start = int(input())
+
+INF = 9999999
+v_list = [INF]*(V+1)
+visit = [0]*(V+1)
+
+
+line = [[] for i in range(V+1)]
+for i in range(E):
+    s,e,a = map(int, input().split())
+    line[s].append([e,a])
+
+def find():
+    min = INF
+    index = 0
+    for i in range(1,V+1):
+        if visit[i]: continue
+        
+        if v_list[i] < min:
+            min = v_list[i]
+            index = i
+    return index
+        
+def solve(start):
+    for i in line[start]:
+        v_list[i[0]] = v_list[i[0]] if v_list[i[0]] <i[1] else i[1]         
+        
+    v_list[start] = 0
+    visit[start] = 1
+    for i in range(V-1):
+        index = find()
+        visit[index] = 1
+        for v in line[index]:
+            if visit[v[0]]: continue
+            if v_list[index]+v[1] <  v_list[v[0]]:
+                v_list[v[0]] = v_list[index] + v[1]
+
+solve(start)
+for v in v_list[1:]:
+    print("INF") if v == INF else print(v)
