14d61290814b00a7e24232693b0d8c844e6b483a
parksey <parkseyeon99@naver.com>
2022-10-05 23:24:39 +0900

    2022-10-04

---
+++ dinamic/11727.py
@@ -0,0 +1,16 @@
+N = int(input())
+dp = [0]*N
+def solve():
+    if N==1:
+        return 1
+    dp[0] = 1
+    
+    if N==2:
+        return 3
+    dp[1] = 3
+    
+    for i in range(2, N):
+        dp[i] = dp[i-1] + dp[i-2]*2
+    return dp[N-1]
+    
+print(solve())
