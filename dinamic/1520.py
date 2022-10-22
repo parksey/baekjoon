15f1058e101fe16166c9fc8daf4ab66b8de5ee10
parksey <parkseyeon99@naver.com>
2022-10-22 19:34:40 +0900

    2022-10-22

--- dinamic/1520.py
+++ dinamic/1520.py
@@ -1,36 +1,23 @@
-from collections import deque
-from tabnanny import check
-
-
 N, M = map(int, input().split())
 
 gMap = [list(map(int, input().split())) for _ in range(N)]
+dir = [[0,-1], [-1,0], [0,1], [1,0]]
+checkMap = [[0 for _ in range(M)] for __ in range(N)]
+checkMap[N-1][M-1] = 1
 
-def solve():
-    checkMap = [[0 for _ in range(M)] for __ in range(N)]
-    checkMap[0][0] = 1
-    dir = [[0,-1], [-1,0], [0,1], [1,0]]
+def dp(x, y):
+    if checkMap[y][x] != 0:
+        return checkMap[y][x]
     
-    stack = [[0,0]]
-    cnt = 0
-    while stack:
-        top = stack.pop()
-        
-        if top[0] == N and top[1] == M:
-            continue
-        
-        for d in dir:
-            y = d[0]+top[0]; x=d[1]+top[1]
+    for d in dir:
+            nextY = d[0]+y; nextX=d[1]+x
             
-            if y >=N or y<0 or x>=M or x<0:
+            if nextY >=N or nextY<0 or nextX>=M or nextX<0:
                 continue
             
-            if gMap[y][x] < gMap[top[0]][top[1]]:
-                stack.append([y,x])
-                checkMap[y][x] = 1
-                
-    for i in range(N):
-        print(*checkMap[i])
-    return checkMap[N-1][M-1]
+            if gMap[y][x] > gMap[nextY][nextX]:
+                checkMap[y][x] += dp(nextX,nextY) 
+    return checkMap[y][x]
 
-print(solve())
+# print(dp(M-1, N-1))
+print(dp(0,0))
