aacde4389cb513fada86e4cc0413f28ae0317d58
parksey <parkseyeon99@naver.com>
2023-04-12 21:51:26 +0900

    2023-04-12_program_70129

---
+++ java/programmers/src/main/java/level2/P70129.java
@@ -0,0 +1,38 @@
+package level2;
+
+import java.util.stream.IntStream;
+
+public class P70129 {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while(!s.equals("1")) {
+            long nextValue = getValue(s);
+            answer[1] += (s.length() - nextValue);
+            answer[0]++;
+            s = getBinary(nextValue);
+        }
+
+
+        return answer;
+    }
+
+    public long getValue(String s) {
+        return IntStream.range(0, s.length())
+                .mapToObj(i -> s.charAt(i))
+                .filter(c->c=='1')
+                .count();
+    }
+
+    public String getBinary(long value) {
+        StringBuilder sb = new StringBuilder();
+
+        while (value > 1) {
+            sb.append(value % 2);
+            value /= 2;
+        }
+        sb.append(value);
+
+        return sb.reverse().toString();
+    }
+}
--- java/programmers/src/test/java/level2/Level2Test.java
+++ java/programmers/src/test/java/level2/Level2Test.java
@@ -1,6 +1,9 @@
 package level2;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -10,5 +13,13 @@ public class Level2Test {
         P12941 p = new P12941();
         assertThat(p.solution(new int[]{1,4,2}, new int[]{5,4,4})).isEqualTo(29);
         assertThat(p.solution(new int[]{1,2}, new int[]{3,4})).isEqualTo(10);
+    }
+
+    @Test
+    public void p70129() {
+        P70129 p = new P70129();
+        assertThat(p.solution("110010101001")).isEqualTo(new int[]{3,8});
+        assertThat(p.solution("01110")).isEqualTo(new int[]{3,3});
+        assertThat(p.solution("1111111")).isEqualTo(new int[]{4,1});
     }
 }
